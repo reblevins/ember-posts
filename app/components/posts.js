51274cf685d22b9b8ee6aa58f288460e3839bcfb
Rodney Blevins <rodney@blevins.nl>
2020-07-21 16:57:54 -0600

    pagination

--- app/components/posts.js
+++ app/components/posts.js
@@ -5,8 +5,18 @@ import { action } from '@ember/object';
 export default class PostsComponent extends Component {
     @tracked showDetail = false;
     numberPosts = 20;
-    
+    @tracked currentPost = 0;
+
+    get posts() {
+        console.log(this.args);
+        let { posts } = this.args
+        return posts.slice(this.currentPost, this.numberPosts)
+    }
+
     @action toggleDetail() {
         this.showDetail = !this.showDetail
+    }
+    @action next() {
+        this.currentPost = this.currentPost + this.numberPosts
     }
 }
